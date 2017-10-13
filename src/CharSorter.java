c087727f5a46762db1b4144c67f74765ea8ccc7f
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2017-10-12 21:16:59 -0400

    Added a method to take in user input and check it for errors

--- src/CharSorter.java
+++ src/CharSorter.java
@@ -1,9 +1,13 @@
+//Note: some of this code was copied from my Project 1 code
+
+import java.util.*;
+
 public class CharSorter
 {
     //The goal of this method is to sort all of the
     //characters in alphabetical order.
     //Argument and return types can be changed.
-    public static String[] alphabeticalSort (String[])
+    public static String[] alphabeticalSort (String[] var)
     {
 
     }
@@ -11,7 +15,7 @@ public class CharSorter
     //The goal of this method is to sort all of the
     //characters from highest to lowest frequency.
     //Argument and return types can be changed.
-    public static String[] frequencySort (String[])
+    public static String[] frequencySort (String[] var)
     {
 
     }
@@ -20,8 +24,68 @@ public class CharSorter
     //Textual characters, Numeric characters, WhiteSpace characters,
     //and Symbolic characters.
     //Argument and return types can be changed.
-    public static String[] charTypes (String[])
+    public static String[] charTypes (String[] var)
     {
+
+    }
+
+    //The goal of this method is to print the options to the
+    //user, catch any errors in the input and return the user's
+    //choice to the main method.
+    public static int printOptionsAndCatchErrors()
+    {
+        int userChoice = 0;
+        Boolean error = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please select the option you would like to see");
+        System.out.println("");
+        System.out.println("1. Display character frequencies alphabetically");
+        System.out.println("2. Display sorted frequencies");
+        System.out.println("3. Show types of character frequencies");
+        System.out.println("4. Exit");
+
+
+        while (error)
+        {
+            try
+            {
+                userChoice = scanner.nextInt();
+
+                if (userChoice < 1)
+                {
+                    System.out.println("\nError, bad input, please enter a number 1-4");
+                }
+                else if (userChoice > 4)
+                {
+                    System.out.println("\nError, bad input, please enter a number 1-4");
+                }
+                else
+                {
+                    error = false;
+                    return userChoice;
+                }
+            }
+            catch (InputMismatchException notAnInt)
+            {
+                System.out.println("\nError, bad input, please enter a number 1-4");
+            }
+            catch (Exception except)
+            {
+                System.out.println("\nError, bad input, please enter a number 1-4");
+            }
+
+
+            scanner.nextLine();
+
+            System.out.println("Please select the option you would like to see");
+            System.out.println("");
+            System.out.println("1. Display character frequencies alphabetically");
+            System.out.println("2. Display sorted frequencies");
+            System.out.println("3. Show types of character frequencies");
+            System.out.println("4. Exit");
+        }
+        return userChoice;
 
     }
 
