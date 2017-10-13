4da79d6a2c4ac1dd60e46721c47a6ec9c251b1eb
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2017-10-12 20:47:28 -0400

    Initial code outlining methods from the instructions

---
+++ src/CharSorter.java
@@ -0,0 +1,32 @@
+public class CharSorter
+{
+    //The goal of this method is to sort all of the
+    //characters in alphabetical order.
+    //Argument and return types can be changed.
+    public static String[] alphabeticalSort (String[])
+    {
+
+    }
+
+    //The goal of this method is to sort all of the
+    //characters from highest to lowest frequency.
+    //Argument and return types can be changed.
+    public static String[] frequencySort (String[])
+    {
+
+    }
+
+    //The goal of this method is to sort the data into four categories:
+    //Textual characters, Numeric characters, WhiteSpace characters,
+    //and Symbolic characters.
+    //Argument and return types can be changed.
+    public static String[] charTypes (String[])
+    {
+
+    }
+
+    public static void main (String[] args)
+    {
+
+    }
+}
