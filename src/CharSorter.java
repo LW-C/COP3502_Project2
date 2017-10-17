de72e713e79bd7c4021c4f7e2054f10b35949162
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2017-10-16 20:13:45 -0400

    Updated the ascii array with the characters in order

--- src/CharSorter.java
+++ src/CharSorter.java
@@ -113,13 +113,115 @@ public class CharSorter
     {
         Scanner scanner = new Scanner(System.in);
         Boolean runAgain = true;
+        int[] frequencies = new int[95];
+        char[] ascii = new char[95];
         int choice = 1;
         String userInput;
 
+        //Put all of the variables in the ascii array
+        ascii[0]  = ' ';
+        ascii[1]  = '!';
+        ascii[2]  = '"';
+        ascii[3]  = '#';
+        ascii[4]  = '$';
+        ascii[5]  = '%';
+        ascii[6]  = '&';
+        ascii[7]  = '\'';
+        ascii[8]  = '(';
+        ascii[9]  = ')';
+        ascii[10] = '*';
+        ascii[11] = '+';
+        ascii[12] = ',';
+        ascii[13] = '-';
+        ascii[14] = '.';
+        ascii[15] = '/';
+        ascii[16] = '0';
+        ascii[17] = '1';
+        ascii[18] = '2';
+        ascii[19] = '3';
+        ascii[20] = '4';
+        ascii[21] = '5';
+        ascii[22] = '6';
+        ascii[23] = '7';
+        ascii[24] = '8';
+        ascii[25] = '9';
+        ascii[26] = ':';
+        ascii[27] = ';';
+        ascii[28] = '<';
+        ascii[29] = '=';
+        ascii[30] = '>';
+        ascii[31] = '?';
+        ascii[32] = '@';
+        ascii[33] = 'A';
+        ascii[34] = 'B';
+        ascii[35] = 'C';
+        ascii[36] = 'D';
+        ascii[37] = 'E';
+        ascii[38] = 'F';
+        ascii[39] = 'G';
+        ascii[40] = 'H';
+        ascii[41] = 'I';
+        ascii[42] = 'J';
+        ascii[43] = 'K';
+        ascii[44] = 'L';
+        ascii[45] = 'M';
+        ascii[46] = 'N';
+        ascii[47] = 'O';
+        ascii[48] = 'P';
+        ascii[49] = 'Q';
+        ascii[50] = 'R';
+        ascii[51] = 'S';
+        ascii[52] = 'T';
+        ascii[53] = 'U';
+        ascii[54] = 'V';
+        ascii[55] = 'W';
+        ascii[56] = 'X';
+        ascii[57] = 'Y';
+        ascii[58] = 'Z';
+        ascii[59] = '[';
+        ascii[60] = '\\';
+        ascii[61] = ']';
+        ascii[62] = '^';
+        ascii[63] = '_';
+        ascii[64] = '`';
+        ascii[65] = 'a';
+        ascii[66] = 'b';
+        ascii[67] = 'c';
+        ascii[68] = 'd';
+        ascii[69] = 'e';
+        ascii[70] = 'f';
+        ascii[71] = 'g';
+        ascii[72] = 'h';
+        ascii[73] = 'i';
+        ascii[74] = 'j';
+        ascii[75] = 'k';
+        ascii[76] = 'l';
+        ascii[77] = 'm';
+        ascii[78] = 'n';
+        ascii[79] = 'o';
+        ascii[80] = 'p';
+        ascii[81] = 'q';
+        ascii[82] = 'r';
+        ascii[83] = 's';
+        ascii[84] = 't';
+        ascii[85] = 'u';
+        ascii[86] = 'v';
+        ascii[87] = 'w';
+        ascii[88] = 'x';
+        ascii[89] = 'y';
+        ascii[90] = 'z';
+        ascii[91] = '{';
+        ascii[92] = '|';
+        ascii[93] = '}';
+        ascii[94] = '~';
+
+        //Take the string input from the user
         System.out.println("Welcome to Character Sorter Program");
         System.out.println("Please input a string to be sorted");
         userInput = scanner.nextLine();
-        //Send userInput to a method that records the frequencies of each character
+
+        //Send userInput to a recordFrequencies to get the frequencies for each character
+        frequencies = recordFrequencies(userInput, ascii);
 
         while (runAgain)
         {
@@ -129,18 +231,26 @@ public class CharSorter
              *  Carry out the choice that the user selects
              **/
 
+            choice = printOptionsAndCatchErrors();
 
             //What to do based on user's choice
-            if (choice == 1) {
+            if (choice == 1)
+            {
                 //Display the character frequencies alphabetically
 
-            } else if (choice == 2) {
+            }
+            else if (choice == 2)
+            {
                 //Display the sorted frequencies
 
-            } else if (choice == 3) {
+            }
+            else if (choice == 3)
+            {
                 //Show types of character frequencies
 
-            } else if (choice == 4) {
+            }
+            else if (choice == 4)
+            {
                 //Exit sorter
                 System.out.println("Character Sorter Exited Successfully");
                 runAgain = false;
